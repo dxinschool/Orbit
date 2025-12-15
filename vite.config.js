4618872361b682f5057d7c27ecc31918e72e0891
dxinschool <124847139+dxinschool@users.noreply.github.com>
2025-12-15 21:59:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 // If your app will be served from https://<user>.github.io/Orbit/
 // set base to '/Orbit/'. Update if your repo name differs.
 export default defineConfig({
-  base: '/Orbit/'
+  base: 'dxinschool.github.io/Orbit/'
 });
